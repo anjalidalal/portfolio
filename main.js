b97e453c5cc6dc896640e66c946756c470a6c059
anjalidalal <anjalimaria2002@gmail.com>
2022-01-07 21:55:37 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+// <!-- <div class="r">
+// <div class="icon">
+//   <a href="mailto:anjalimaria2002@gmail.com" target="_blank">
+//     <i class='fa fa-envelope'>  anjalimaria2002@gmail.com</i>
+//   </a>
+// </div>
+// <div class="icon">
+//   <a href="https://www.linkedin.com/in/anjali-dalal-768b73205/" target="_blank">
+//   <i class='fa fa-linkedin'>  Anjali Dalal</i>
+//   </a>
+// </div>
+// </div> -->
